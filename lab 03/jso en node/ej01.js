eb4a49450d747e071d36e05f8670b721c6fd64de
danielsct56bm <danielsct56bm@gmail.com>
2022-09-13 13:45:50 -0500

    js en node

---
+++ lab 03/jso en node/ej01.js
@@ -0,0 +1,12 @@
+var nombre = "Carlos";
+console.log(nombre);
+
+var numero = 1000;
+console.log(numero);
+
+var objecto = {
+    nombre : "Juan",
+    apellido : "Perez",
+    telefono : "55443321"
+}
+console.log(objecto);
